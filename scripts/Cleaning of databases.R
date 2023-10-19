598bc4b2c4df620d1e898a32ac6277b9bfddb5ac
kirdol <lodrik.adam@hotmail.ch>
2023-10-19 17:37:53 +0200

    Ajout de dataset pour D2

---
+++ scripts/Cleaning of databases.R
@@ -0,0 +1,25 @@
+
+### 1 Main Table
+D1
+
+### 2 World Index
+D2_1_Unemployment_rate <- read.csv(here("DS-project/scripts/data/UNE_2EAP_SEX_AGE_RT_A-full-2023-10-19.csv"))
+D2_2_Cost_of_living
+D2_3_Crime_index
+
+# 3 World bank data on GDP population and military
+D3
+
+# 4 Internet Usage
+D4_0_Internet_usage <- read.csv(here("DS-project/scripts/data/share-of-individuals-using-the-internet-2.csv"))
+
+# 5 The Human Freedom Index
+D5
+
+# 6 Natural Disasters
+
+# 7 COVID-19 Pandemic Data Variables
+
+# 8 Financial crisis information and many more
+
+# 9 List of countries in conflicts from 1999 to 2019
