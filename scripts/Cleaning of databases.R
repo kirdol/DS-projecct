d7f65e9d73b4a4dc4131e49ce3d9f153ba504545
kirdol <lodrik.adam@hotmail.ch>
2023-10-19 18:20:06 +0200

    adding country code on D2.1
    j'ai ajouté une colonne avec les code des pays au dataset D2.1 Unemployment rate

--- scripts/Cleaning of databases.R
+++ scripts/Cleaning of databases.R
@@ -3,8 +3,8 @@
 
 ### 2 World Index
 D2_1_Unemployment_rate <- read.csv(here("DS-project/scripts/data/UNE_2EAP_SEX_AGE_RT_A-full-2023-10-19.csv"))
-D2_2_Cost_of_living
-D2_3_Crime_index
+D2_2_Cost_of_living # TBF
+D2_3_Crime_index # TBF
 
 # 3 World bank data on GDP population and military
 
@@ -20,3 +20,10 @@ D4_0_Internet_usage <- read.csv(here("DS-project/scripts/data/share-of-individua
 # 8 Financial crisis information and many more
 
 # 9 List of countries in conflicts from 1999 to 2019
+
+# Modification
+countries_codes <- D4_0_Internet_usage %>%
+  select(Entity, Code) %>%
+  distinct()
+D4_0_Internet_usage_with_country_code <-
+  merge(D2_1_Unemployment_rate, countries_codes[, c("Entity", "Code")], by.x = "ref_area.label", by.y = "Entity", all.x = TRUE)
