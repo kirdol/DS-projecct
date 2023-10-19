2c7134726714d5df121b66df016490ab88d2bb41
kirdol <lodrik.adam@hotmail.ch>
2023-10-19 17:38:52 +0200

    Update Cleaning of databases.R

--- scripts/Cleaning of databases.R
+++ scripts/Cleaning of databases.R
@@ -1,6 +1,5 @@
 
 ### 1 Main Table
-D1
 
 ### 2 World Index
 D2_1_Unemployment_rate <- read.csv(here("DS-project/scripts/data/UNE_2EAP_SEX_AGE_RT_A-full-2023-10-19.csv"))
@@ -8,13 +7,11 @@ D2_2_Cost_of_living
 D2_3_Crime_index
 
 # 3 World bank data on GDP population and military
-D3
 
 # 4 Internet Usage
 D4_0_Internet_usage <- read.csv(here("DS-project/scripts/data/share-of-individuals-using-the-internet-2.csv"))
 
 # 5 The Human Freedom Index
-D5
 
 # 6 Natural Disasters
 
