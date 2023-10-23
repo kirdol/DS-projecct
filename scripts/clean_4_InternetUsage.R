a243e6b75ba0a12376d453285fdcf8cf5358bf55
deliafuchs19 <148160167+deliafuchs19@users.noreply.github.com>
2023-10-23 12:34:16 +0200

    modifs

--- scripts/clean_4_InternetUsage.R
+++ scripts/clean_4_InternetUsage.R
@@ -6,5 +6,5 @@ D4_0_Internet_usage <- # import the dataset
 D4_0_Internet_usage <- # make sure that we have a datafraame
   as.data.frame(D4_0_Internet_usage)
 
-D2_1_Unemployment_rate <- # keep only the data after 2000 to match the main datast
+D2_1_Unemployment_rate <- # keep only the data after 2000 to match the main dataset
   D2_1_Unemployment_rate[D2_1_Unemployment_rate$time >= 2000, ]
