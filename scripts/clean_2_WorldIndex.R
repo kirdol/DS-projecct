c6cac3e247ff005a9eafcbf67daf604b3f610f70
kirdol <lodrik.adam@hotmail.ch>
2023-10-23 17:35:12 +0200

    Update clean_2_WorldIndex.R

--- scripts/clean_2_WorldIndex.R
+++ scripts/clean_2_WorldIndex.R
@@ -3,7 +3,7 @@
 D2_1_Unemployment_rate <- # import the dataset
   read.csv(here("scripts","data","UNE_2EAP_SEX_AGE_RT_A-full-2023-10-19.csv"))
 
-xD2_1_Unemployment_rate <- # make sure that we have a datafraame
+D2_1_Unemployment_rate <- # make sure that we have a datafraame
   as.data.frame(D2_1_Unemployment_rate)
 
 # Here, we use the dataset of the internet usage to extract the code of the countries.
@@ -19,6 +19,29 @@ D2_1_Unemployment_rate <- # We merge the dataset of Unemployment rate with codes
 
 D2_1_Unemployment_rate <- # keep only the data after 2000 to match the main datast
   D2_1_Unemployment_rate[D2_1_Unemployment_rate$time >= 2000, ]
+
+# remove colums that we do not need
+D2_1_Unemployment_rate <- D2_1_Unemployment_rate %>%
+  select(-source.label, -obs_status.label)
+
+# rearrange the columns
+D2_1_Unemployment_rate <- D2_1_Unemployment_rate %>%
+  select(Code, ref_area.label, time, indicator.label, sex.label, classif1.label, obs_value)
+
+# rename columns
+D2_1_Unemployment_rate <- D2_1_Unemployment_rate %>%
+  rename(
+    code = Code,
+    country = ref_area.label,
+    year = time,
+    age_cat = classif1.label
+  )
+
+# drop ligns with sex indications and remove the column linked to sex
+D2_1_Unemployment_rate <- D2_1_Unemployment_rate %>%
+  filter(!str_detect(sex.label, fixed("Male")) & !str_detect(sex.label, fixed("Female")))
+D2_1_Unemployment_rate <- D2_1_Unemployment_rate %>%
+  select(-sex.label)
 
 ## Cost of living
 D2_2_Cost_of_living # TBD
