1d89bb9de5292da154e65ba308e1515c74b3783f
kirdol <lodrik.adam@hotmail.ch>
2023-11-05 18:58:34 +0100

    Update clean_4_InternetUsage.R

--- scripts/clean_4_InternetUsage.R
+++ scripts/clean_4_InternetUsage.R
@@ -24,6 +24,5 @@ D4_0_Internet_usage$`internet usage` <-
   D4_0_Internet_usage$`internet usage` / 100
 
 # Keep only the countries that are in our main dataset
-
 D4_0_Internet_usage <- D4_0_Internet_usage %>% filter(code %in% list_country)
 length(unique(D4_0_Internet_usage$code))
