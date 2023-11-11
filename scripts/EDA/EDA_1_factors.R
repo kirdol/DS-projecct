a7808c15bd06c7e4fd704c3869a214413bf49658
StefanFavre <stefan.favre@hotmail.com>
2023-11-11 20:18:49 +0100

    Create EDA_1_factors.R

---
+++ scripts/EDA/EDA_1_factors.R
@@ -0,0 +1,4 @@
+##### How do complex structures, systems, relationships and events influence a country's SDG scores?  #####
+
+data_question1 <- read.csv(here("scripts","data","data_question1.csv"))
+
