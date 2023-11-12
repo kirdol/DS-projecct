8bef882f142b4446a2056709d18200b24f2be0da
StefanFavre <stefan.favre@hotmail.com>
2023-11-12 14:18:52 +0100

    Q1 investigations

--- scripts/EDA/EDA_1_factors.R
+++ scripts/EDA/EDA_1_factors.R
@@ -33,9 +33,66 @@ ggplot(data = cor_melted, aes(Var1, Var2, fill = value)) +
         axis.text.y = element_text(size = 8)) +
   coord_fixed() +
   labs(x = '', y = '', title = 'Correlation Matrix Heatmap')
+#why does goal1 and goal10 NA values since cor_matrix? 
+
+#### Check influence of variables on scores ####
+
+# Define the dependent variables (replace with your actual variables)
+dependent_vars <- c("goal1", 
+                    "goal2", 
+                    "goal3", 
+                    "goal4",
+                    "goal5",
+                    "goal6",
+                    "goal7",
+                    "goal8",
+                    "goal9",
+                    "goal10",
+                    "goal11",
+                    "goal12",
+                    "goal13",
+                    "goal14",
+                    "goal15",
+                    "goal16",
+                    "goal17" )
+
+# Define the independent variables
+independent_vars <- c( "unemployment.rate", "GDPpercapita" ,                
+                      "MilitaryExpenditurePercentGDP", "internet.usage",              "pf_law",                      "pf_security",               "pf_movement",                  
+                      "pf_religion",                  "pf_assembly",                   "pf_expression",                 "pf_identity",                   "ef_government",                
+                      "ef_legal",                     "ef_money",                      "ef_trade",                      "ef_regulation")
+
+# Function to run linear regression
+run_regression <- function(dep_var, indep_vars) {
+  formula <- as.formula(paste(dep_var, "~", paste(indep_vars, collapse = " + ")))
+  return(summary(lm(formula, data = Correlation_overall)))
+}
+
+# Store results
+results <- list()
+
+# Loop over combinations
+for (dep_var in dependent_vars) {
+  for (i in 1:length(independent_vars)) {
+    combn_vars <- combn(independent_vars, i, simplify = FALSE)
+    for (vars in combn_vars) {
+      model_summary <- run_regression(dep_var, vars)
+      results[[paste(dep_var, paste(vars, collapse = "_"), sep = "_")]] <- model_summary
+    }
+  }
+}
+
+# Now, 'results' contains the summaries of all regressions
+
+
+# Check if certain region and/or continents have better/worse scores for each goal
+
+# What is in average the goal with the lowest score? With the highest score? Which goals have the highest disparities amongst countries/region?
 
 
 
+
+write.csv(Correlation_overall, file = here("scripts","data","Correlation_overall.csv"))
 
 
 
