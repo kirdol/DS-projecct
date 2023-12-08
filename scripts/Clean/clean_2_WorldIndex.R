4dd7a468355cec3fec0f7ce08765560d461f5bc0
kirdol <lodrik.adam@hotmail.ch>
2023-12-08 11:19:45 +0100

    stuff

--- scripts/Clean/clean_2_WorldIndex.R
+++ scripts/Clean/clean_2_WorldIndex.R
@@ -9,7 +9,7 @@ D2_1_Unemployment_rate <- read.csv(here("scripts","data","UnemploymentRate.csv")
     age_category = classif1.label,
     sex = sex.label
   ) %>%
-  select(-ref_area.label, -time, -obs_value, -classif1.label, -sex.label, -source.label, -obs_status.label, -indicator.label) %>%
+  select(-ref_area.label, -time, -obs_value, -classif1.label, -sex.label, -source.label, -obs_status.lMASSabel, -indicator.label) %>%
   merge(D1_0_SDG_country_list[, c("country", "code")], by = "country", all.x = TRUE) %>%
   filter(year >= 2000 & year <= 2022,
          !str_detect(sex, fixed("Male")) & !str_detect(sex, fixed("Female")),
